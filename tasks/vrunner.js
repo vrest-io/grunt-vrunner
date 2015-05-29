8e387633524d492f7ddc8af0d1263660e97e69d7
Ramesh Kumar <ramesh.kumar@optimizory.com>
2015-05-29 16:44:24 +0530

    chalk

--- tasks/vrunner.js
+++ tasks/vrunner.js
@@ -8,28 +8,46 @@
 
 'use strict';
 
-var vrunner = require('vrunner');
+var vrunner = require('vrunner'), chalk = require('chalk');
 
 module.exports = function(grunt) {
 
   grunt.registerTask('vrunner', 'Runs vREST test cases.', function() {
     var done = this.async();
-    var over = function(err){
+    var over = function(err,dontExit){
       if(!err) err = 'An unknown glitch found.';
       if(!Array.isArray(err)) err = [err];
       err.forEach(function(error){
         console.log(error);
       });
-      done();
+      if(!dontExit) done();
     };
     var options = this.options();
-    (new vrunner(options)).run(function(err,report,remarks){
+    var Runner = (new vrunner(options));
+    Runner.run(function(err,report,remarks){
       if(err) over(err);
       else {
         grunt.log.ok('EXECUTION OF ALL TEST CASES SUCCESSFULLY COMPLETED.');
         console.log(remarks,report);
         done();
       }
+    });
+    Runner.on('testcase',function(pass,tc,trtc){
+      if(pass){
+        grunt.log.writeln(tc.method+' --> '+tc.url+' --> '+chalk.green('PASSED'));
+      } else if(pass === false){
+        grunt.log.writeln(tc.method+' --> '+tc.url+' --> '+chalk.red.bold('FAILED'));
+      } else {
+        grunt.log.writeln(tc.method+' --> '+tc.url+' --> '+chalk.yellow('NOT_EXECUTED'));
+      }
+    });
+    Runner.on('error',function(err){
+      grunt.log.error();
+      over(err);
+    });
+    Runner.on('warning',function(warning){
+      console.log(chalk.cyan('WARNING'));
+      over(warning,true);
     });
   });
 };
