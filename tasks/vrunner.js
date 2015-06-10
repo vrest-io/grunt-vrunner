e1b2856b3ee16f2bc1519c0edd527d2eb3272bac
Dheeraj Aggarwal <dheerajcompsc@gmail.com>
2015-06-10 18:55:18 +0530

    display improvements done

--- tasks/vrunner.js
+++ tasks/vrunner.js
@@ -11,6 +11,27 @@
 var vrunner = require('vrunner'), chalk = require('chalk');
 
 module.exports = function(grunt) {
+  
+  //add right side padding
+  var rpad = function(str, totalSize, padChar){
+    if(!padChar) padChar = ' ';
+    if(str.length >= totalSize) return str;
+    else {
+      for(var i = str.length; i < totalSize; i++){
+        str += padChar;
+      }
+      return str;
+    }
+  };
+
+  var getMethodName = function(method){
+    if(method === "GET") return chalk.blue.bold(method) + '    ';
+    else if(method === "POST") return chalk.bold(method) + '   ';
+    else if(method === "PUT") return chalk.green.bold(method) + '    ';
+    else if(method === "PATCH") return chalk.magenta.bold(method) + '  ';
+    else if(method === "DELETE") return chalk.red.bold(method) + ' ';
+    else return chalk.bold(method) + ' ';
+  };
 
   grunt.registerTask('vrunner', 'Runs vREST test cases.', function() {
     var done = this.async();
@@ -27,27 +48,42 @@ module.exports = function(grunt) {
     Runner.run(function(err,report,remarks){
       if(err) over(err);
       else {
-        grunt.log.ok('EXECUTION OF ALL TEST CASES SUCCESSFULLY COMPLETED.');
+        //grunt.log.ok('EXECUTION OF ALL TEST CASES SUCCESSFULLY COMPLETED.');
         console.log(remarks,report);
         done();
       }
     });
+    
+    var index = 1;
     Runner.on('testcase',function(pass,tc,trtc){
+      //console.log(trtc.);
+      var prefix = rpad(index + '.', 5) + getMethodName(tc.method);
+      index++;
       if(pass){
-        grunt.log.writeln(tc.method+' --> '+tc.url+' --> '+chalk.green('PASSED'));
+        grunt.log.writeln(prefix + chalk.green.bold(tc.summary || tc.url) + ' (' + trtc.executionTime + 'ms) ');
       } else if(pass === false){
-        grunt.log.writeln(tc.method+' --> '+tc.url+' --> '+chalk.red.bold('FAILED'));
+        grunt.log.writeln(prefix + chalk.red.bold(tc.summary || tc.url) + ' (' + trtc.executionTime + 'ms) ');
       } else {
-        grunt.log.writeln(tc.method+' --> '+tc.url+' --> '+chalk.cyan('NOT_EXECUTED'));
+        grunt.log.writeln(prefix + chalk.cyan.bold('[Not Executed] ' + (tc.summary || tc.url)) + ' (' + trtc.executionTime + 'ms) ');
       }
     });
+    
     Runner.on('error',function(err){
       grunt.log.error();
       over(err);
     });
+
+    Runner.on('log', function(message, level){
+      if(!level) level = 'info';
+      message = '>> ' + message;
+      if(level === 'error') message = chalk.red.bold(message);
+      else message = chalk.blue.bold(message);
+      console.log(message);
+    });
+
     Runner.on('warning',function(warning){
-      console.log(chalk.yellow('WARNING'));
-      over(warning,true);
+      //console.log(chalk.yellow('WARNING'));
+      //over(warning,true);
     });
   });
 };
